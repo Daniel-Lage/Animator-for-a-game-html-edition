b2e1adb32dbbf7719bc48a39613d61083d473578
Daniel Lage <daniellc3221@gmail.com>
2022-08-25 15:28:38 -0300

    Update script.js
    added image loading and rectangle list saving functionalities

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const frame_rate = 60;
 
+var image;
 var isdrawing = false;
-
 var starting_corner;
 var ending_corner;
-
-var rectangles = [];
+var rectangles = {};
 var rect_id = 0;
 
 function mousemove(e) {
@@ -35,7 +34,7 @@ function mousedown(e) {
   isdrawing = true;
 }
 
-function mouseup(e) {
+function mouseup() {
   if (isdrawing) {
     isdrawing = false;
     prev_rect = document.getElementById("0");
@@ -50,36 +49,43 @@ function mouseup(e) {
       Math.abs(starting_corner[0] - ending_corner[0]),
       Math.abs(starting_corner[1] - ending_corner[1]),
     ];
-    rectangles.push(rect);
     rect_id++;
     draw_rect(rect);
   }
 }
 
 function mouseout(e) {
-  if (e.toElement.tagName != "rect" && e.toElement.tagName != "svg") {
-    if (isdrawing) {
-      isdrawing = false;
-      prev_rect = document.getElementById("0");
-      prev_rect.remove();
-      rect = [
-        starting_corner[0] < ending_corner[0]
-          ? starting_corner[0]
-          : ending_corner[0],
-        starting_corner[1] < ending_corner[1]
-          ? starting_corner[1]
-          : ending_corner[1],
-        Math.abs(starting_corner[0] - ending_corner[0]),
-        Math.abs(starting_corner[1] - ending_corner[1]),
-      ];
-      rectangles.push(rect);
-      rect_id++;
-      draw_rect(rect);
-    }
+  if (
+    e.toElement &&
+    e.toElement.tagName != "rect" &&
+    e.toElement.tagName != "svg"
+  ) {
+    mouseup();
   }
 }
 
 function draw_rect(rect) {
   canvas = document.getElementById("canvas");
+  rectangles[rect_id] = rect;
+  console.log(rectangles);
   canvas.innerHTML += `<rect id = "${rect_id}" x="${rect[0]}" y="${rect[1]}" width="${rect[2]}" height="${rect[3]}" stroke="black" stroke-width="2" fill="none"/>`;
+}
+
+function load() {
+  const canvas = document.getElementById("canvas");
+  const image_loader = document.getElementById("loader");
+  image = image_loader.value.split("\\").pop();
+  canvas.style.backgroundImage = `url("images/${image}")`;
+}
+
+function save() {
+  var rect_list = [];
+  for (var key in rectangles) {
+    rect_list.push(rectangles[key]);
+  }
+  const a = document.createElement("a");
+  const blob = new Blob([JSON.stringify(rect_list)]);
+  a.href = URL.createObjectURL(blob);
+  a.download = `${image.split(".")[0]}.json`;
+  a.click();
 }
